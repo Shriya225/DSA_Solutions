0e0e332522282fca5cb370adac4bcc36d46856a5
Shriya Palle <shripalle225@gmail.com>
2025-06-22 18:14:16 +0530

    Revsing Recursion

---
+++ BinaryTree.py/Tree_Traversals.py
@@ -0,0 +1,47 @@
+
+class TreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+root=TreeNode(1)
+n2=TreeNode(2)
+n3=TreeNode(3)
+n4=TreeNode(4)
+n5=TreeNode(5)
+n6=TreeNode(6)
+n7=TreeNode(7)
+root.left=n2
+root.right=n3
+n2.left=n4
+n2.right=n5
+n3.left=n6
+n3.right=n7
+
+def preOrder(root):
+    if not root:
+        return
+    print(root.data,end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+
+def inOrder(root):
+    if not root:
+        return
+    inOrder(root.left)
+    print(root.data,end=" ")
+    inOrder(root.right)
+
+def postOrder(root):
+    if not root:
+        return
+    postOrder(root.left)
+    postOrder(root.right)
+    print(root.data,end=" ")
+    
+preOrder(root)
+print()
+inOrder(root)
+print()
+postOrder(root)
