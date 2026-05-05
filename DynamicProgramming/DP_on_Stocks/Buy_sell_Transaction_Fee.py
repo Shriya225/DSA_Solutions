ebd3183d4889f0ffcf2b31eb9814a09b3aa35d8d
Shriya Palle <shripalle225@gmail.com>
2026-05-05 16:07:07 +0530

    Completed DP_on_Stocks

--- DynamicProgramming/DP_on_Stocks/Buy_sell_Transaction_Fee.py
+++ DynamicProgramming/DP_on_Stocks/Buy_sell_Transaction_Fee.py
@@ -1,29 +1,30 @@
-# 309. Best Time to Buy and Sell Stock with Cooldown
+# 714. Best Time to Buy and Sell Stock with Transaction Fee
 
+# Make  any no.of transations
 
 # reccurence rel'n
-def bs2(a,i,buy):
-    if i>=len(a):
+def bs2(a,i,buy,f):
+    if i==len(a):
         return 0
     ans=float("-inf")
     if buy:
         ans=max(-a[i]+bs2(a,i+1,False),bs2(a,i+1,True))
     else:
-        ans=max(a[i]+bs2(a,i+2,True),bs2(a,i+1,False))
+        ans=max(a[i]-f+bs2(a,i+1,True),bs2(a,i+1,False))
     return ans
 
 # memoization
 class Solution:
-    def maxProfit(self, a):
+    def maxProfit(self, a,f):
         def bs2(a,i,buy,dp):
-            if i>=len(a):
+            if i==len(a):
                 return 0
             if dp[i][buy]!=-1:return dp[i][buy]
             ans=float("-inf")
             if buy:
                 ans=max(-a[i]+bs2(a,i+1,False,dp),bs2(a,i+1,True,dp))
             else:
-                ans=max(a[i]+bs2(a,i+2,True,dp),bs2(a,i+1,False,dp))
+                ans=max(a[i]-f+bs2(a,i+1,True,dp),bs2(a,i+1,False,dp))
             dp[i][buy]=ans
             return ans
         dp=[[-1]*(2) for _ in range(len(a))]
@@ -31,8 +32,8 @@ class Solution:
     
 
 # Tabulation
-def bs2_tab(a):
-    dp=[[0]*(2) for _ in range(len(a)+2)]
+def bs2_tab(a,f):
+    dp=[[-1]*(2) for _ in range(len(a)+1)]
 
     # base case
     dp[-1][0]=0
@@ -43,32 +44,20 @@ def bs2_tab(a):
         for j in range(2):
             if j==1:
                 ans=max(-a[i]+dp[i+1][0],dp[i+1][1])
+                
             else:
-                ans=max(a[i]+dp[i+2][1],dp[i+1][0])
+                ans=max(a[i]-f+dp[i+1][1],dp[i+1][0])
             dp[i][j]=ans
     return dp[0][1]
 
 
-
 # space optimization
-def maxProfit(a):
-    n = len(a)
-    
-    next1 = [0, 0]  # dp[i+1]
-    next2 = [0, 0]  # dp[i+2]
-
-    for i in range(n-1, -1, -1):
-        curr = [0, 0]
-        
-        # j = 1 (buy)
-        curr[1] = max(-a[i] + next1[0], next1[1])
-        
-        # j = 0 (sell)
-        curr[0] = max(a[i] + next2[1], next1[0])
-        
-        # shift forward
-        next2 = next1
-        next1 = curr
-
-    return next1[1]
-
+def bs2_so(a,f):
+    p1=p2=0
+    # fill dp
+    for i in range(len(a)-1,-1,-1):
+            c_b=max(-a[i]+p1,p2)     
+            c_s=max(a[i]-f+p2,p1)
+            p1=c_s
+            p2=c_b
+    return c_b
