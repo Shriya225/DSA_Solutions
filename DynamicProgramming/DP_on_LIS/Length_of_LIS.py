7e1a048cca4e9d7ee39e78a95810534d6551c7ef
Shriya Palle <shripalle225@gmail.com>
2026-05-09 11:25:19 +0530

    solving LIS

---
+++ DynamicProgramming/DP_on_LIS/Length_of_LIS.py
@@ -0,0 +1,40 @@
+# 300. Longest Increasing Subsequence
+
+def LIS(a, i, l):
+    if i == len(a):
+        return 0
+    
+    # always have the option to skip
+    not_take = LIS(a, i+1, l)
+    
+    take = 0
+    if l==-1 or a[i] > a[l]:
+        take = 1 + LIS(a, i+1, i)
+    
+    return max(take, not_take)
+
+
+# memoization..
+class Solution:
+    def lengthOfLIS(self, a):
+        def LIS(a, i, l,dp):
+            if i == len(a):
+                return 0
+            if dp[i][l]!=-1:return dp[i][l]
+            # always have the option to skip
+            not_take = LIS(a, i+1, l,dp)
+            
+            take = 0
+            if l==-1 or a[i] > a[l]:
+                take = 1 + LIS(a, i+1, i,dp)
+            dp[i][l]=max(take, not_take)
+            return max(take, not_take)
+        dp=[[-1]*len(a) for i in range(len(a))]
+        return LIS(a,0,-1,dp)
+
+
+# Tabulation..
+def lis_tab(a):
+    dp=[[-1]*len(a) for i in range(len(a)+1)]
+
+
